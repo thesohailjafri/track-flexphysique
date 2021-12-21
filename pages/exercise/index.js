f9612bfcf4861342a5f8ccc32bf5871a9f398178
thesohailjafri <thesohailjafri@gmail.com>
2021-12-21 20:49:00 +0530

    add dialog for new exercise

--- pages/exercise/index.js
+++ pages/exercise/index.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
 import Calender from '../../components/util/Calender'
 import LineChart from '../../charts/LineChart'
 import WeightInputBox from '../../components/mini/WeightInputBox'
 import LiftingWeightInputBox from '../../components/mini/LiftingWeightInputBox'
 import ExerciseDropdown from '../../components/mini/ExerciseDropdown'
 import LogNote from '../../components/mini/LogNote'
+import { Dialog, Transition } from '@headlessui/react'
+import { AnimatePresence, motion } from 'framer-motion'
 const data = {
     labels: ['January', 'February', 'March',
         'April', 'May'],
@@ -36,7 +38,17 @@ const chartData1 = {
     data: data,
     options: optionsThree,
 }
-export default function gym() {
+export default function exercise() {
+    let [isNewExerciseDialogOpen, setIsNewExerciseDialogOpen] = useState(false)
+
+
+    function closeModal() {
+        setIsNewExerciseDialogOpen(false)
+    }
+
+    function openModal() {
+        setIsNewExerciseDialogOpen(true)
+    }
     return (
         <div className='grid gap-4 grid-cols-12'
         >
@@ -51,7 +63,9 @@ export default function gym() {
                 <button className='bg-blue-600 text-white p-2 rounded w-80'>Add Log</button>
                 <div className='inline-flex justify-between'>
                     <button className='bg-blue-600 text-white p-2 rounded w-40'>Hide Calculator</button>
-                    <button className='bg-blue-600 text-white p-2 rounded w-36'>Add Exericse</button>
+                    <button className='bg-blue-600 text-white p-2 rounded w-36'
+                        onClick={openModal}
+                    >Add Exericse</button>
                 </div>
 
             </div>
@@ -60,6 +74,71 @@ export default function gym() {
             <div className='--card col-span-full'>
                 <Calender />
             </div>
+
+            <Transition appear show={isNewExerciseDialogOpen} as={Fragment}>
+                <Dialog
+                    as="div"
+                    className="fixed inset-0 z-10 overflow-y-auto"
+                    onClose={closeModal}
+                >
+                    <div className="min-h-screen px-4 text-center">
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0"
+                            enterTo="opacity-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100"
+                            leaveTo="opacity-0"
+                        >
+                            <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
+                        </Transition.Child>
+
+                        {/* This element is to trick the browser into centering the modal contents. */}
+                        <span
+                            className="inline-block h-screen align-middle"
+                            aria-hidden="true"
+                        >
+                            &#8203;
+                        </span>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                                <Dialog.Title
+                                    as="h3"
+                                    className="text-lg font-medium leading-6 text-gray-900"
+                                >
+                                    Payment successful
+                                </Dialog.Title>
+                                <div className="mt-2">
+                                    <p className="text-sm text-gray-500">
+                                        Your payment has been successfully submitted. We’ve sent you
+                                        an email with all of the details of your order.
+                                    </p>
+                                </div>
+
+                                <div className="mt-4">
+                                    <button
+                                        type="button"
+                                        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                                        onClick={closeModal}
+                                    >
+                                        Got it, thanks!
+                                    </button>
+                                </div>
+                            </div>
+                        </Transition.Child>
+                    </div>
+                </Dialog>
+            </Transition>
+
         </div>
     )
 }
