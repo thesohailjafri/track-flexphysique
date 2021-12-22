98e26825512ae84e4a75d680cc48baf81ee3d776
thesohailjafri <thesohailjafri@gmail.com>
2021-12-22 20:15:04 +0530

    i dont know anymore

--- pages/exercise/index.js
+++ pages/exercise/index.js
@@ -110,27 +110,40 @@ export default function exercise() {
                             leaveFrom="opacity-100 scale-100"
                             leaveTo="opacity-0 scale-95"
                         >
-                            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                            <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded">
                                 <Dialog.Title
                                     as="h3"
                                     className="text-lg font-medium leading-6 text-gray-900"
                                 >
-                                    Payment successful
+                                    Add New Exercise
                                 </Dialog.Title>
-                                <div className="mt-2">
-                                    <p className="text-sm text-gray-500">
-                                        Your payment has been successfully submitted. We’ve sent you
-                                        an email with all of the details of your order.
-                                    </p>
+                                <div className="mt-2 flex">
+                                    <input type="text" name="" id=""
+                                        placeholder='Exercise Name'
+                                        className='py-2 rounded focus:outline-none
+                                        border-b-2 border-gray-300 w-full
+                                        ' />
+                                    <input type="text" name="" id=""
+                                        placeholder='Exercise Name'
+                                        className='py-2 rounded focus:outline-none
+                                        border-b-2 border-gray-300 w-full
+                                        ' />
                                 </div>
 
                                 <div className="mt-4">
                                     <button
                                         type="button"
-                                        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                                        className="inline-flex justify-center px-4 mr-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
                                         onClick={closeModal}
                                     >
-                                        Got it, thanks!
+                                        Add
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                                        onClick={closeModal}
+                                    >
+                                        Add
                                     </button>
                                 </div>
                             </div>
